300650f446b5093502e961f06673be2019a80864
Rafael Azevedo <rrazvd@gmail.com>
2021-12-13 15:14:57 -0300

    Add color schemes

---
+++ src/constants/schemes.jsx
@@ -0,0 +1,77 @@
+/**
+ * This file defines the platform color schemes.
+ */
+
+export const lightScheme = {
+  primary: {
+    light: '#FFAC5B',
+    main: '#FA7B00',
+    dark: '#CC6C0F',
+  },
+  secondary: {
+    light: '#748898',
+    main: '#323F4A',
+    dark: '#161615',
+  },
+  neutral: {
+    white: '#FFFFFF',
+    gray: {
+      light: '#DEDEDE',
+      main: '#A4A4A4',
+      dark: '#333333',
+      medium: '#464646',
+    },
+  },
+  stroke: {
+    light: '#F0F1F3',
+    main: '#DFE1E6',
+    dark: '#0B1016',
+  },
+  blue: {
+    light: '#67D9FC',
+    main: '#00B9F1',
+    dark: '#008CB7',
+  },
+  green: {
+    light: '#EEFAA3',
+    main: '#D3F500',
+    dark: '#A1BA09',
+  },
+};
+
+export const darkScheme = {
+  primary: {
+    light: '#FFAC5B',
+    main: '#FA7B00',
+    dark: '#CC6C0F',
+  },
+  secondary: {
+    light: '#1B2935',
+    main: '#323F4A',
+    dark: '#E7EDF2',
+  },
+  neutral: {
+    white: '#FFFFFF',
+    gray: {
+      light: '#DEDEDE',
+      main: '#A4A4A4',
+      dark: '#333333',
+      medium: '#464646',
+    },
+  },
+  stroke: {
+    light: '#151C24',
+    main: '#151C24',
+    dark: '#0D1620',
+  },
+  blue: {
+    light: '#67D9FC',
+    main: '#00B9F1',
+    dark: '#008CB7',
+  },
+  green: {
+    light: '#EEFAA3',
+    main: '#D3F500',
+    dark: '#A1BA09',
+  },
+};
