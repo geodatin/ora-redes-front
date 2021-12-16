15d6a984404ca26c1bb268456b5921514efdf1f0
Matheus Teles <matheustdo1@gmail.com>
2021-12-16 16:14:38 -0300

    Solve pagination logic

--- src/components/Header/HeaderButton/index.jsx
+++ src/components/Header/HeaderButton/index.jsx
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import useStyles from './styles';
 
@@ -9,14 +9,25 @@ import useStyles from './styles';
  * This component renders a HeaderButton
  * @returns Header Button
  */
-export default function HeaderButton({ to, actived, title }) {
+export default function HeaderButton({ to, title }) {
   HeaderButton.propTypes = {
     to: PropTypes.func.isRequired,
-    actived: PropTypes.bool.isRequired,
     title: PropTypes.string.isRequired,
   };
 
   const classes = useStyles();
+  const { pathname } = useLocation();
+
+  const actived = useMemo(() => {
+    const path1 = pathname.substring(1);
+    const path2 = pathname.substring(0, path1.indexOf('/') + 1);
+
+    if (path2) {
+      return path2 === to;
+    }
+
+    return pathname === to;
+  }, [pathname]);
 
   return (
     <Link to={to} className={classes.link}>
