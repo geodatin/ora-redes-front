60632b80d531532a712dbbaabdea65438a467e69
Matheus Teles <matheustdo1@gmail.com>
2022-01-05 15:42:11 -0300

    Edit example values

--- src/pages/PageTwo/index.jsx
+++ src/pages/PageTwo/index.jsx
@@ -42,24 +42,6 @@ export default function PageTwo() {
                         value: 'Agência Nacional de Águas',
                       },
                     ]}
-                    fileTypes={[{ name: 'CSV', color: '#FA7B00' }]}
-                    url={[
-                      {
-                        link: 'https://speed.hetzner.de/100MB.bin',
-                        info: 'Baixar CSV',
-                      },
-                    ]}
-                  />
-                  <FilePost
-                    title="Tabela de estações fluviométricas"
-                    description="Pensando mais a longo prazo, a utilização de recursos de hardware dedicados conduz a um melhor balancemanto de carga da utilização dos serviços nas nuvens. No entanto, não podemos esquecer que a disponibilização de ambientes faz parte de um processo de gerenciamento de memória avançado dos paradigmas de desenvolvimento de software. "
-                    items={[
-                      { key: 'País', value: 'Brasil2' },
-                      {
-                        key: 'Instituição',
-                        value: 'Agência Nacional de Águas2',
-                      },
-                    ]}
                     fileTypes={[
                       { name: 'CSV', color: '#FA7B00' },
                       { name: 'PDF', color: '#A1BA09' },
