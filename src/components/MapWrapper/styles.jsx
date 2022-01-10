46fd703ff8669e28bad01d890d1b47960b0e7a7c
Matheus Teles <matheustdo1@gmail.com>
2022-01-10 15:23:28 -0300

    Add north icon

--- src/components/MapWrapper/styles.jsx
+++ src/components/MapWrapper/styles.jsx
@@ -9,6 +9,13 @@ const useStyles = createUseStyles(() => ({
       marginBottom: 10,
     },
   },
+  northIcon: {
+    position: 'absolute',
+    height: 30,
+    right: 10,
+    top: 10,
+    zIndex: 997,
+  },
 }));
 
 export default useStyles;
