c9bab07b2e8e218ef9a0488adf98b28f49d6d8f4
Matheus Teles <matheustdo1@gmail.com>
2022-01-10 16:18:30 -0300

    Add responsivity to north icon

--- src/components/MapWrapper/styles.jsx
+++ src/components/MapWrapper/styles.jsx
@@ -1,12 +1,23 @@
 import { createUseStyles } from 'react-jss';
 
+import { breakpoints } from '../../constants/constraints';
+
 const useStyles = createUseStyles(() => ({
   mapContainer: {
     position: 'relative',
   },
   itemContainer: {
+    position: 'absolute',
+    left: 10,
+    top: 10,
+
     '& > div': {
       marginBottom: 10,
+    },
+
+    [breakpoints.max.md]: {
+      left: 'initial',
+      right: 10,
     },
   },
   northIcon: {
@@ -15,6 +26,11 @@ const useStyles = createUseStyles(() => ({
     right: 10,
     top: 10,
     zIndex: 997,
+
+    [breakpoints.max.md]: {
+      right: 'initial',
+      left: 10,
+    },
   },
 }));
 
