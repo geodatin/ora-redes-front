5656f3d22f41fbd07249427619561c6521640095
Matheus Teles <matheustdo1@gmail.com>
2022-01-19 16:37:22 -0300

    Create filter component

---
+++ src/components/AdvancedFilter/index.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable no-unused-vars */
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { Autocomplete, TextField } from '@mui/material';
+import PropTypes from 'prop-types';
+import React, { useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useTheme } from 'react-jss';
+
+import useStyles from './styles';
+
+/**
+ * This component renders a advanced filter
+ * @returns Advanced Filter
+ */
+export default function AdvancedFilter({ TextFieldProps, onSelect, ...rest }) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const inputRef = useRef();
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Autocomplete
+      id="search-box"
+      autoHighlight
+      selectOnFocus
+      disableClearable
+      classes={{ paper: classes.paper }}
+      className={classes.root}
+      open={open}
+      onOpen={() => setOpen(true)}
+      onClose={() => {
+        setOpen(false);
+        setInputValue('');
+      }}
+      value={null}
+      inputValue={inputValue}
+      onInputChange={(e, newValue) => setInputValue(newValue)}
+      popupIcon={
+        open ? (
+          <CloseRoundedIcon
+            className={classes.addIcon}
+            style={{ fontSize: 20, color: theme.secondary.dark }}
+          />
+        ) : (
+          <AddRoundedIcon
+            className={classes.addIcon}
+            style={{ fontSize: 20, color: theme.secondary.dark }}
+          />
+        )
+      }
+      onChange={(e, newValue) => onSelect(newValue)}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          inputRef={inputRef}
+          margin="none"
+          size="small"
+          placeholder={t('components.advancedFilter.placeholder')}
+          variant="outlined"
+          classes={{ root: classes.textfield }}
+          fullWidth
+          {...TextFieldProps}
+        />
+      )}
+      groupBy={(option) => option.type}
+      {...rest}
+    />
+  );
+}
+
+AdvancedFilter.defaultProps = {
+  TextFieldProps: {},
+};
+
+AdvancedFilter.propTypes = {
+  TextFieldProps: PropTypes.shape(),
+  onSelect: PropTypes.func.isRequired,
+};
---
+++ src/components/AdvancedFilter/styles.jsx
@@ -0,0 +1,45 @@
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles((theme) => ({
+  root: {
+    '& .MuiAutocomplete-endAdornment': {
+      top: 8,
+    },
+  },
+  textfield: {
+    '& .MuiAutocomplete-inputRoot[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-child':
+      {
+        paddingLeft: 10,
+        paddingRight: 30,
+        width: '100%',
+      },
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: theme.neutral.gray.main,
+      borderRadius: 5,
+    },
+  },
+  addIcon: {
+    height: 5,
+  },
+  paper: {
+    backgroundColor: 'green',
+
+    '&.MuiPaper-root': {
+      marginTop: 5,
+      backgroundColor: theme.background.popup,
+      fontSize: 16,
+    },
+    '& .MuiListSubheader-root': {
+      paddingLeft: 10,
+    },
+    '& .MuiAutocomplete-groupUl .MuiAutocomplete-option': {
+      paddingLeft: 10,
+    },
+    '& .MuiAutocomplete-groupLabel': {
+      fontSize: 12.8,
+      backgroundColor: theme.background.popup,
+    },
+  },
+}));
+
+export default useStyles;
