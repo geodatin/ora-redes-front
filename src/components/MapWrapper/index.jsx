ef57cbc002f4fa64197e7b0b07694c7a9d76f73b
Matheus Teles <matheustdo1@gmail.com>
2022-01-27 16:00:17 -0300

    Add map darkmode

--- src/components/MapWrapper/index.jsx
+++ src/components/MapWrapper/index.jsx
@@ -1,10 +1,12 @@
 import L from 'leaflet';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { useTheme } from 'react-jss';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css';
 import GeodatinLogo from '../../assets/images/geodatin-map.svg';
 import NorthIcon from '../../assets/images/north.svg';
+import { darkScheme } from '../../constants/schemes';
 import useStyles from './styles';
 import ZoomButton from './ZoomButton';
 
@@ -16,6 +18,10 @@ export default function MapWrapper() {
   const position = [-5.0800011, -61.3420118];
   const classes = useStyles();
   const itemsRef = useRef();
+  const theme = useTheme();
+  const [map, setMap] = useState();
+  const lightTileRef = useRef();
+  const darkTileRef = useRef();
 
   useEffect(() => {
     if (itemsRef?.current) {
@@ -24,16 +30,38 @@ export default function MapWrapper() {
     }
   }, []);
 
+  useEffect(() => {
+    if (map && lightTileRef.current && darkTileRef.current) {
+      if (theme === darkScheme) {
+        lightTileRef.current.remove();
+        darkTileRef.current.addTo(map);
+        console.log('dark');
+      } else {
+        darkTileRef.current.remove();
+        lightTileRef.current.addTo(map);
+        console.log('light');
+      }
+    }
+  }, [theme, map]);
+
   return (
     <MapContainer
+      whenCreated={setMap}
       className={classes.mapContainer}
       center={position}
       zoom={5}
       zoomControl={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        ref={lightTileRef}
+        attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <TileLayer
+        ref={darkTileRef}
+        attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        className={classes.tileLayer}
       />
       <div ref={itemsRef} className={classes.itemContainer}>
         <ZoomButton />
