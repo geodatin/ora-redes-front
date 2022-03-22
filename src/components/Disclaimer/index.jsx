c3720d12a1d1842f87f3efda3b40a8663d49975a
Rafael Azevedo <rrazvd@gmail.com>
2022-03-22 17:53:03 -0300

    Update disclaimer content

--- src/components/Disclaimer/index.jsx
+++ src/components/Disclaimer/index.jsx
@@ -24,22 +24,31 @@ export default function Disclaimer() {
       button={{ isEnabled: true, text: t('specific.disclaimer.button.text') }}
     >
       <Typography variant="body">
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
-        over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites of the word in classical literature, discovered the
-        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of
-        ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section
-        1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is
-        reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
-        de Finibus Bonorum et Malorum by Cicero are also reproduced in their
-        exact original form, accompanied by English versions from the 1914
-        translation by H. Rackham.
+        {t('specific.disclaimer.content.p1')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p2')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p3')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p4')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p5')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p6')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p7')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p8')}
+        <br />
+        <br />
+        {t('specific.disclaimer.content.p9')}
       </Typography>
     </CustomDialog>
   );
